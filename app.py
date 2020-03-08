4a1d4eec4d97f09849b67fbcd8d65c9a1d0d3401
Sivalingam <1386576@zone1.scb.net>
2020-03-08 22:32:45 +0530

    master

--- app.py
+++ app.py
@@ -1,91 +1,24 @@
-# Python standard libraries
+import functools
 import json
 import os
-import sqlite3
-from datetime import datetime
-import re
 
-# Third-party libraries
-from flask import Flask, redirect, request, url_for
-from flask_login import (
-    LoginManager,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
-)
-from oauthlib.oauth2 import WebApplicationClient
-import requests
-from flask import Flask, render_template
+import flask
 
-# Internal imports
-from db import init_db_command
-from user import User
+from authlib.client import OAuth2Session
+import google.oauth2.credentials
+import googleapiclient.discovery
 
-# Configuration
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
+import google_auth
 
-# Flask app setup
-app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
-@app.route("/")
+app = flask.Flask(__name__)
+app.secret_key = "SivaRamdom123"
+
+app.register_blueprint(google_auth.app)
+
+@app.route('/')
 def index():
-    if current_user.is_authenticated:
-        return (
-            "<p>Hello, {}! You're logged in! Email: {}</p>"
-            "<div><p>Google Profile Picture:</p>"
-            '<img src="{}" alt="Google profile pic"></img></div>'
-            '<a class="button" href="/logout">Logout</a>'.format(
-                current_user.name, current_user.email, current_user.profile_pic
-            )
-        )
-    else:
-        return '<a class="button" href="/login">Google Login</a>'
+    if google_auth.is_logged_in():
+        user_info = google_auth.get_user_info()
+        return '<div>You are currently logged in as ' + user_info['given_name'] + '<div><pre>' + json.dumps(user_info, indent=4) + "</pre>"
 
-def get_google_provider_cfg():
-    return requests.get(GOOGLE_DISCOVERY_URL).json()
-
-@app.route("/login")
-def login():
-    # Find out what URL to hit for Google login
-    google_provider_cfg = get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
-
-    # Use library to construct the request for Google login and provide
-    # scopes that let you retrieve user's profile from Google
-    request_uri = client.prepare_request_uri(
-        authorization_endpoint,
-        redirect_uri=request.base_url + "/callback",
-        scope=["openid", "email", "profile"],
-    )
-    return redirect(request_uri)
-
-@app.route("/login/callback")
-def callback():
-    # Get authorization code Google sent back to you
-    code = request.args.get("code")
-
-# Find out what URL to hit to get tokens that allow you to ask for
-# things on behalf of a user
-google_provider_cfg = get_google_provider_cfg()
-token_endpoint = google_provider_cfg["token_endpoint"]
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("index"))
-
-# New functions
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
+    return 'You are not currently logged in.'
