5c5a6d97349866c20ad1cb64edae2181480ccabd
Sivalingam <1386576@zone1.scb.net>
2020-03-09 20:37:24 +0530

    master

--- app.py
+++ app.py
@@ -2,23 +2,40 @@ import functools
 import json
 import os
 
-import flask
+from flask import Flask, render_template
 
 from authlib.client import OAuth2Session
 import google.oauth2.credentials
 import googleapiclient.discovery
 
 import google_auth
+from flask.templating import render_template
+from werkzeug.utils import redirect
+from flask.helpers import url_for
 
-app = flask.Flask(__name__)
+
+app = Flask(__name__)
+
 app.secret_key = "SivaRamdom123"
 
 app.register_blueprint(google_auth.app)
 
-@app.route('/')
-def index():
+@app.route('/',methods = ['POST', 'GET'])
+def home():
     if google_auth.is_logged_in():
         user_info = google_auth.get_user_info()
-        return '<div>You are currently logged in as ' + user_info['given_name'] + '<div><pre>' + json.dumps(user_info, indent=4) + "</pre>"
+        r = json.dumps(user_info)
+        loaded_r = json.loads(r)
+        return render_template("index.html",message='yes',userin=loaded_r['email'])
 
-    return 'You are not currently logged in.'
+    return render_template("index.html",message='You are not currently logged in.',userin=None)
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
--- google_auth.py
+++ google_auth.py
@@ -10,7 +10,7 @@ import googleapiclient.discovery
 ACCESS_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
 AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent'
 
-AUTHORIZATION_SCOPE ='openid email profile'
+AUTHORIZATION_SCOPE = 'openid email profile'
 
 AUTH_REDIRECT_URI = "http://localhost:5000/auth"
 BASE_URI = "http://localhost:5000/"
@@ -22,30 +22,34 @@ AUTH_STATE_KEY = 'auth_state'
 
 app = flask.Blueprint('google_auth', __name__)
 
+
 def is_logged_in():
     return True if AUTH_TOKEN_KEY in flask.session else False
+
 
 def build_credentials():
     if not is_logged_in():
         raise Exception('User must be logged in')
 
     oauth2_tokens = flask.session[AUTH_TOKEN_KEY]
-    
+
     return google.oauth2.credentials.Credentials(
-                oauth2_tokens['access_token'],
-                refresh_token=oauth2_tokens['refresh_token'],
-                client_id=CLIENT_ID,
-                client_secret=CLIENT_SECRET,
-                token_uri=ACCESS_TOKEN_URI)
+        oauth2_tokens['access_token'],
+        refresh_token=oauth2_tokens['refresh_token'],
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        token_uri=ACCESS_TOKEN_URI)
+
 
 def get_user_info():
     credentials = build_credentials()
 
     oauth2_client = googleapiclient.discovery.build(
-                        'oauth2', 'v2',
-                        credentials=credentials)
+        'oauth2', 'v2',
+        credentials=credentials)
 
     return oauth2_client.userinfo().get().execute()
+
 
 def no_cache(view):
     @functools.wraps(view)
@@ -58,19 +62,21 @@ def no_cache(view):
 
     return functools.update_wrapper(no_cache_impl, view)
 
+
 @app.route('/google/login')
 @no_cache
 def login():
     session = OAuth2Session(CLIENT_ID, CLIENT_SECRET,
                             scope=AUTHORIZATION_SCOPE,
                             redirect_uri=AUTH_REDIRECT_URI)
-  
+
     uri, state = session.authorization_url(AUTHORIZATION_URL)
 
     flask.session[AUTH_STATE_KEY] = state
     flask.session.permanent = True
 
     return flask.redirect(uri, code=302)
+
 
 @app.route('/auth')
 @no_cache
@@ -80,19 +86,20 @@ def google_auth_redirect():
     if req_state != flask.session[AUTH_STATE_KEY]:
         response = flask.make_response('Invalid state parameter', 401)
         return response
-    
+
     session = OAuth2Session(CLIENT_ID, CLIENT_SECRET,
                             scope=AUTHORIZATION_SCOPE,
                             state=flask.session[AUTH_STATE_KEY],
                             redirect_uri=AUTH_REDIRECT_URI)
 
     oauth2_tokens = session.fetch_access_token(
-                        ACCESS_TOKEN_URI,            
-                        authorization_response=flask.request.url)
+        ACCESS_TOKEN_URI,
+        authorization_response=flask.request.url)
 
     flask.session[AUTH_TOKEN_KEY] = oauth2_tokens
 
     return flask.redirect(BASE_URI, code=302)
+
 
 @app.route('/google/logout')
 @no_cache
